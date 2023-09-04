b8075a14d370a1afb0d1a5959e08bab725952bfb
reshad21 <miakala7@gmail.com>
2023-09-04 23:27:21 +0600

    array,object

---
+++ array.js
@@ -0,0 +1,21 @@
+var x = 10;
+var y = 20;
+
+var number = [10, 20, 40, 30, 88, 33];
+
+var vowel = ['a', 'e', 'i', '0', 'u'];
+
+var myFriends = ["jown", "dow", "smith", "lna"];
+
+// console.log(myFriends.length);
+// var firstFriendName = myFriends[0];
+
+myFriends[0] = "jihad";
+
+myFriends.push("villas");
+myFriends.push("tomas");
+myFriends.push("rosey");
+
+myFriends.pop("tomas");
+
+console.log(myFriends);
---
+++ object.js
@@ -0,0 +1,7 @@
+var person1 = {
+    name:"john",
+    age: 30,
+    address:"usa",
+    skill:["c++","js","html","css","responsive"],
+}
+console.log(person1.name);
