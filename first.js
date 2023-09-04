e727d946c1347148d08f1ca2bedb43c662eb48a3
reshad21 <miakala7@gmail.com>
2023-09-04 21:29:14 +0600

    first commit

---
+++ first.js
@@ -0,0 +1,19 @@
+console.log("jihad");
+var name = "jihad student";
+// +,-,*,/,%
+
+// true,false
+
+var x = 20;
+var y = 20;
+
+
+// >,<,!==, !, <=,>=
+
+&&,||,!
+
+var result = x>=y;
+
+console.log(result);
+// var a = a+1;
+
