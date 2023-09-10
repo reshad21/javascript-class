a4fe25f2b6f53ad1c6322388e8012f099e1c8f25
reshad21 <miakala7@gmail.com>
2023-09-10 20:54:19 +0600

    object class

--- array.js
+++ array.js
@@ -1,5 +1,7 @@
-var x = 10;
+const x = 10;
 var y = 20;
+
+console.log(x);
 
 var number = [10, 20, 40, 30, 88, 33];
 
@@ -7,7 +9,8 @@ var vowel = ['a', 'e', 'i', '0', 'u'];
 
 var myFriends = ["jown", "dow", "smith", "lna"];
 
-// console.log(myFriends.length);
+
+console.log(myFriends.length);
 // var firstFriendName = myFriends[0];
 
 myFriends[0] = "jihad";
--- object.js
+++ object.js
@@ -1,7 +1,76 @@
 var person1 = {
-    name:"john",
+    name: "john",
     age: 30,
-    address:"usa",
-    skill:["c++","js","html","css","responsive"],
+    address: "usa",
+    skill: ["c++", "js", "html", "css", "responsive"],
+    
 }
-console.log(person1.name);
+var person2 = {
+    name: "Dow",
+    age: 32,
+    address: "usa",
+    skill: ["c++", "js", "html", "css", "responsive"],
+}
+var person3 = {
+    name: "Liam",
+    age: 37,
+    address: "usa",
+    skill: ["c++", "js", "html", "css", "responsive"],
+}
+
+var data = [person1, person2, person3];
+console.log(data);
+
+var personDataCollection = [
+    {
+        name: "john",
+        age: 30,
+        address: "usa",
+        skill: ["c++", "js", "html", "css", "responsive"],
+    },
+    {
+        name: "Dow",
+        age: 32,
+        address: "usa",
+        skill: ["c++", "js", "html", "css", "responsive"],
+    },
+    {
+        name: "Liam",
+        age: 37,
+        address: "usa",
+        skill: ["c++", "js", "html", "css", "responsive"],
+    }
+
+]
+
+console.log(personDataCollection.length);
+
+
+// console.log(person1.skill[0]);
+
+// var married = "yes";
+// var updatePerson1 = { ...person1, married };
+// console.log(updatePerson1);
+
+// var country="country";
+// person1[country] = "Bangladesh";
+// console.log(person1);
+
+// person1.nationality = "Bangladesh";
+// person1.name="dow";
+// console.log(person1);
+
+
+// const updatePerson1 = { ...person1, married };
+// var person1Phone = 193823920;
+// person1.person1Phone
+// console.log(updatePerson1);
+// person1.phoneNumber = 1289322123;
+
+// var companyName = "companyName";
+// person1[companyName] = "microsoft";
+// const updatePerson1 = { ...person1, wifeName: "malinda" };
+// console.log(person1);
+
+
+
