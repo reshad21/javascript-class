6630cce039deb97b1b263ac43adb4ac8b0253da5
reshad21 <miakala7@gmail.com>
2023-09-25 22:13:30 +0600

    for and ifelse condition

---
+++ ifelse.js
@@ -0,0 +1,52 @@
+// var mark = 90;
+
+// if (mark >= 80) {
+//     console.log("You got A+");
+// }
+// else if (mark >= 79) {
+//     console.log("You got A");
+// }
+// else if (mark >= 69) {
+//     console.log("You got A-");
+// }
+// else if (mark >= 59) {
+//     console.log("You got B");
+// }
+// else if (mark >= 49) {
+//     console.log("You got c");
+// }
+// else if (mark >= 33) {
+//     console.log("You got D");
+// }
+// else {
+//     console.log("Not pass");
+// }
+
+// var num1 = 50;
+// var num2 = 20;
+// var num3 = 40;
+
+// if ((num1 > num2) && (num1 > num3)) {
+//     console.log(num1,"is bigger");
+// }
+// else if((num2 > num1) && (num2 > num3)){
+//     console.log(num2,"is bigger");
+// }
+// else{
+//     console.log(num3,"is bigger");
+// }
+
+
+// initialization, condition, increment/decrement
+
+// a++, a--
+
+// var a = 10;
+// let b = 20;
+//  b = 30;
+
+for (let i = 0; i <= 10; i++) {
+    console.log("zihad");
+    
+}
+
