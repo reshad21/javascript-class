742d5df6b1ad41ddc774cfe457f54f90eaef4b09
reshad21 <miakala7@gmail.com>
2023-10-12 22:25:23 +0600

    function and loop

--- ifelse.js
+++ ifelse.js
@@ -45,8 +45,49 @@
 // let b = 20;
 //  b = 30;
 
-for (let i = 0; i <= 10; i++) {
-    console.log("zihad");
-    
+// num = [1,2,3,4,5,6,7,8,9,10]
+
+// for (let i = 1; i <= 10; i++) {
+
+//     if (i % 2 !== 0) {
+//         console.log(i);
+//     }
+
+// }
+
+// num = [8, 9, 10, 33, 44, 21, 1, 2, 3, 4, 5, 6, 7]
+// console.log(num[0]);
+// console.log(num.length);
+// for (let i = 0; i <= num.length; i++) {
+//     console.log(num[i]);
+// }
+
+// for (let i = 0; i <= 10; i++) {
+
+//     if (i == 7) {
+//         continue
+//     }
+//     console.log(i);
+// }
+
+
+// initialization, condition, increment/decrement
+
+// let i = 0;
+// while (i <= 10) {
+//     console.log(i);
+//     i++;
+// }
+
+
+
+function myBio(name, verityName, workingPlace, skills) {
+    console.log(`my name is ${name}`);
+    console.log(`i completed my graduation in ${verityName} university`);
+    console.log(`now i work on ${workingPlace} marketplace`);
+    console.log(`my skill is ${skills}`);
 }
 
+myBio("reshad", "global", "fiverr", "frontend web developer");
+
+myBio("zihad","global", "upwork", "web design");
